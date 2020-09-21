da35594aec12694b53d6930b48cf6de3b27c7969
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-09-21 13:23:50 -0700

    added code to deal with missing events if service is down (data store of events)

--- query/index.js
+++ query/index.js
@@ -1,19 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 const port = 4002;
 
 const posts = {};
-
-app.get("/posts", (req, res) => res.send("Hello World!"));
-// when a post created it will send to event bus the event and the event bus emits event to all other services
-// and query service handles storing what needs for user to query
-app.post("/events", function (req, res) {
-	const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
 	if (type === "PostCreated") {
 		const { id, title } = data;
 
@@ -33,5 +28,22 @@ app.post("/events", function (req, res) {
 		comment.status = status;
 		comment.content = content;
 	}
+};
+app.get("/posts", (req, res) => res.send("Hello World!"));
+// when a post created it will send to event bus the event and the event bus emits event to all other services
+// and query service handles storing what needs for user to query
+app.post("/events", function (req, res) {
+	const { type, data } = req.body;
+	handleEvent(type, data);
+	res.send({});
 });
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, async () => {
+	console.log(`Example app listening on port ${port}!`);
+	// when use axios the result will be available on the results data property
+	const res = await axios.get("http://localhost:4005/events");
+
+	for (let event of res.data) {
+		console.log("processing event", event.type);
+		handleEvent(event.type, event.data);
+	}
+});
